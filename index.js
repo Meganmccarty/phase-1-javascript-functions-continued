280bab990a229bc3a714d971c5ccbd0b1f98e9a3
Megan McCarty <megan_mccarty@hotmail.com>
2021-05-17 14:11:10 -0400

    commit tree 3c887a6ba5bb84db36f6d65775ec83fe52369905

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Your code here
+// Function declaration
+function saturdayFun(doActivity) {
+    if (doActivity) {
+        return `This Saturday, I want to ${doActivity}!`;
+    } else {
+        return 'This Saturday, I want to roller-skate!';
+    }   
+}
+
+// Function expression
+const mondayWork = function (doWork) {
+    if (doWork) {
+        return `This Monday, I will ${doWork}.`
+    } else {
+        return 'This Monday, I will go to the office.'
+    }
+}
+
+// Functions within functions
+
+// The code below passes all of the tests, but I want to refactor it
+// (Not so many if/else statements)
+/*
+function wrapAdjective(flair) {
+    if (flair) {
+        return function(adjective) {
+            const start = 'You are';
+            if (adjective) {
+                return `${start} ${flair}${adjective}${flair}!`;
+            } else {
+                return `${start} ${flair}special${flair}!`
+            }
+            
+        }
+    } else {
+        return function(adjective) {
+            const start = 'You are';
+            if (adjective) {
+                return `${start} *${adjective}*!`;
+            } else {
+                return `${start} *special*!`
+            }
+            
+        }
+    }   
+}
+*/
+
+function wrapAdjective(flair = '*') {
+    return function(adjective = 'special') {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
